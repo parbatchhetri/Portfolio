2e340c18c1a33f3243e396632f888e6d657c42b6
parbat <parbat.chhetri79@gmail.com>
2023-06-01 13:17:18 +0545

    add

--- src/components/Middlebar.jsx
+++ src/components/Middlebar.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import{ AiOutlineInstagram,AiFillLinkedin} from "react-icons/ai"
+import{ VscGithubAlt} from "react-icons/vsc"
+import {BsSend} from 'react-icons/bs'
 const Middlebar = () => {
   return (
-    <div></div>
+    <div className='flex '>
+    <div className='flex flex-col gap-8 pl-40 pt-60'>
+   <AiOutlineInstagram className=' text-xl'/>
+     <AiFillLinkedin className=' text-xl'/>
+      <VscGithubAlt className=' text-xl'/>
+      </div>
+     <div className='pt-36 pl-28'>
+      <div className='flex flex-col gap-6'>
+      <h1 className='font-semibold text-5xl'>Parvat</h1>
+      <h1 className='font-semibold text-5xl'>Chhetri 👨‍🎓</h1>
+     </div>
+     <div className='  flex items-center gap-2 pt-7'>
+     <hr className=' border-t border-black w-16'></hr>
+    <h1 className=''>Software Engineer</h1>
+    </div>
+    <p className='pt-3 text-gray-500 w-[350px]'>I'm creative Programmer based in chitwan and I'm very passionate and dedicated to my work. </p>
+    <div className='w-[150px] h-[60px] bg-black rounded-2xl mt-11 flex items-center  gap-2 justify-center'>
+      <h1 className='font-semibold text-white'>Say Hello</h1>
+      <BsSend className=' text-white text-xl'/>
+     </div>
+       </div>
+
+     
+    </div>
   )
 }
 
