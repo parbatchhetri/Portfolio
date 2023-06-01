8480c19c690ac425ca3a29143d1c521ee610fbce
parbat <parbat.chhetri79@gmail.com>
2023-06-01 15:45:32 +0545

    add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Middlebar from "./components/Middlebar";
 import Timeline from "./components/Timeline";
 import Aboutme from "./components/Aboutme";
 import Skills from "./components/Skills";
+import Services from "./components/Services";
+import Qualification from './components/Qualification';
 function App() {
   return (
     <div>
@@ -14,6 +16,8 @@ function App() {
       <Timeline />
       <Aboutme />
       <Skills/>
+      <Services/>
+      <Qualification/>
 
     </div>
   )
