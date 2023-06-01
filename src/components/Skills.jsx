16815e800e5a01b96fab62321f55ae369c8d9fc3
parbat <parbat.chhetri79@gmail.com>
2023-06-01 16:05:43 +0545

    add

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div></div>
+    <div>
+      <div className='flex flex-col items-center mt-36 gap-2'>
+        <h1 className='font-bold text-4xl'>Skills</h1>
+        <p className='text-sm tex-white'>My technical level</p>
+        </div>
+        <div className='flex gap-10 mt-14 justify-center'>
+          <div className='w-[300px] h-[300px] bg-white rounded-2xl border-[2px]'>
+
+          </div>
+          <div className='w-[300px] h-[300px] bg-white rounded-2xl border-[2px]'>
+
+          </div>
+        </div>
+    </div>
   )
 }
 
