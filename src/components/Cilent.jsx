88701f6b8cad467013d8d53dcadf718dab6a29a9
parbat <parbat.chhetri79@gmail.com>
2023-06-05 11:28:18 +0545

    add

--- src/components/Cilent.jsx
+++ src/components/Cilent.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Cilent = () => {
   return (
     <div>
-        
+         <div className="flex flex-col items-center mt-36 gap-2">
+        <h1 className="font-bold text-3xl">My Client Say</h1>
+        <p className="text-[12px] text-gray-500 text-xs">Testimonial</p>
+      </div> 
+      <div className="flex gap-10 mt-12 justify-center">
+      <div className="w-[300px] h-[200px] bg-white rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
+      <div className="w-[300px] h-[200px] bg-white rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
+    </div>
     </div>
   )
 }
