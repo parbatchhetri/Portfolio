c456710878b3e02e717f15d28436ce9147473f78
parbat <parbat.chhetri79@gmail.com>
2023-06-05 12:13:48 +0545

    add

--- src/components/Cilent.jsx
+++ src/components/Cilent.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 const Cilent = () => {
   return (
     <div>
-         <div className="flex flex-col items-center mt-36 gap-2">
+      <div className="flex flex-col items-center mt-36 gap-2">
         <h1 className="font-bold text-3xl">My Client Say</h1>
         <p className="text-[12px] text-gray-500 text-xs">Testimonial</p>
-      </div> 
-      <div className="flex gap-10 mt-12 justify-center">
-      <div className="w-[300px] h-[200px] bg-white rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
-      <div className="w-[300px] h-[200px] bg-white rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
-    </div>
-    </div>
-  )
-}
+      </div>
 
-export default Cilent
+
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper  ">
+        <SwiperSlide >
+          <div className="flex justify-center items-center gap-5 mt-10">
+            <div className="w-[250px] h-[150px] bg-white rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
+            <div className="w-[250px] h-[150px] bg-black rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="flex justify-center items-center gap-5 mt-10">
+            <div className="w-[250px] h-[150px] bg-white rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
+            <div className="w-[250px] h-[150px] bg-black rounded-2xl border-[2px] flex flex-col items-center justify-center gap-3 "></div>
+        </div>
+        </SwiperSlide>
+        
+      
+       
+      </Swiper>
+    </div>
+  );
+};
+
+export default Cilent;
